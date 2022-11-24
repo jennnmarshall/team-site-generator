5fc55d0b1eb1e901d7376dc7b1469c29d32cdaa5
Jenn Marshall <jennyfever@gmail.com>
2022-11-23 22:27:29 -0700

    added mockup html

---
+++ index.js
@@ -0,0 +1,7 @@
+const Inquirer = require('Inquirer');
+const fs = require('fs');
+const Employee = require('./lib/employee');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+
