8a4d14a97777f5eec14e971cfe6ca9e48356194b
Jenn Marshall <jennyfever@gmail.com>
2022-11-24 21:00:02 -0700

    added icons to mockup html

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// required exports and external packages
 const Inquirer = require('Inquirer');
 const fs = require('fs');
 const Employee = require('./lib/employee');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const Manager = require('./lib/manager');
+
+// begin inquirer prompts
+
+
 
