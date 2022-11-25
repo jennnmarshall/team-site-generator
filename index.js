d4de8fba4b2a2b1719704d3b8a513160d17978c0
Jenn Marshall <jennyfever@gmail.com>
2022-11-25 15:20:27 -0700

    added additional inquirer prompts

--- index.js
+++ index.js
@@ -1,12 +1,168 @@
 // required exports and external packages
-const Inquirer = require('Inquirer');
-const fs = require('fs');
-const Employee = require('./lib/employee');
-const Engineer = require('./lib/engineer');
-const Intern = require('./lib/intern');
-const Manager = require('./lib/manager');
+const Inquirer = require("Inquirer");
+const fs = require("fs");
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+const { default: inquirer } = require("Inquirer");
 
 // begin inquirer prompts
 
+// global arrays to contain inquirer responses
+const myTeam = [];
+
+// this function initializes the program by asking for team name, then jumping to the addManager function
+function startProject() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "Hello! To generate your team website, begin by entering your Project Name",
+        name: "projectName",
+      },
+    ])
+    .then(response => {
+      const nameObj = response.projectName;
+      myTeam.push(nameObj);
+      addManager();
+    });
+}
+
+// function prompts manager info, adds to array, and prompts additional team members
+function addManager() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your project manager's name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is your project manager's email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is your project manager's employee ID?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is your project manager's office number?",
+      name: "officeNumber",
+    },
+  ]).then (response => {
+    const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
+    myTeam.push(manager);
+    addTeamMember();
+  })
+}
 
 
+function addTeamMember() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Would you like to add additional team members?",
+        choices: ["Add Engineer", "Add Intern", "That's the whole team!"],
+        name: "teamAdd"
+      },
+    ])
+    .then((response) => {
+        switch (response.teamAdd) {
+            case "Add Engineer":
+                addEngineer();
+                break;
+
+            case "Add Intern":
+                addIntern();
+                break;
+                
+            case "That's the whole team!":
+                finalTeam();
+                break;
+        }
+    });    
+}
+
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your Engineer's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your Engineer's email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your Engineer's employee ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your Engineer's Github username?",
+        name: "github",
+      },
+    ])
+    .then((response) => {
+      const engineer = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      myTeam.push(engineer);
+      addTeamMember();
+    });
+}
+
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your Intern's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your Intern's email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your Intern's employee ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What school does your Intern attend?",
+        name: "school",
+      },
+    ])
+    .then((response) => {
+      const intern = new Intern(
+        response.name,
+        response.id,
+        response.email,
+        response.school
+      );
+      myTeam.push(intern);
+      addTeamMember();
+    });
+}
+
+function finalTeam() {
+    console.log("Your team's website has been generated! You can access it in the dist directory.");
+    fs.writeFile(`./dist/${myTeam[0]}.html`, generateSite(myTeam));
+}
+
+startProject();
